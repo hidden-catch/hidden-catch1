2dce0b1d8f1a102e07fc18a339cae66703152c9a
ted111712 <ted111712@gmail.com>
2025-11-25 19:13:10 +0900

    chore: GCP 프로젝트 ID 값 설정 추가

--- src/backend/app/core/config.py
+++ src/backend/app/core/config.py
@@ -29,6 +29,9 @@ class Settings(BaseSettings):
     celery_broker_url: str = "redis://localhost:6379/0"
     celery_result_backend: str = "redis://localhost:6379/0"
 
+    # GCP
+    gcp_project_id: str = ""
+
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", case_sensitive=False, extra="ignore"
     )
