8494cb9ac6b0135ca383d8536f522a7cad1f2bfe
kapustin <kapustin4613@gmail.com>
2025-11-26 18:46:49 +0900

    fix

--- src/backend/app/celery_app.py
+++ src/backend/app/celery_app.py
@@ -3,8 +3,8 @@ from app.core.config import settings
 
 celery_app = Celery(
     "hidden_catch",
-    broker=settings.redis_url,
-    backend=settings.redis_url,
+    broker=settings.celery_broker_url,
+    backend=settings.celery_result_backend,
 )
 
 celery_app.conf.update(
